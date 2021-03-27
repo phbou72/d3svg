840549b0d72a27daa92a5fe09d557c58bca26391
Philippe Bourdages <philippe.bourdages@gmail.com>
2021-03-26 21:02:25 -0400

    Add dots

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,32 @@ const Styling = createGlobalStyle`
         stroke: steelblue;
         stroke-width: 2px;
     }
+
+    div.tooltip {
+        position: absolute;
+        text-align: center;
+        width: 60px;
+        height: 28px;
+        padding: 2px;
+        font: 12px sans-serif;
+        background: lightsteelblue;
+        border: 0px;
+        border-radius: 8px;	
+        pointer-events: none;
+    }
 `;
 
 const createGraph = async () => {
-    const margin = { top: 20, right: 20, bottom: 50, left: 70 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
+    const margin = { top: 20, right: 20, bottom: 50, left: 70 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+    const xScale = d3.scaleTime().range([0, width]);
+    const yScale = d3.scaleLinear().range([height, 0]);
 
     const valueLine = d3
         .line<IDateAmount>()
-        .x((d) => (d?.date ? x(d.date) : 0))
-        .y((d) => y(d.close));
-
-    const svg = d3
-        .select("#chart")
-        .html("")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        .x((d) => (d?.date ? xScale(d.date) : 0))
+        .y((d) => yScale(d.close));
 
     let data = dateAmount.split("\n").map((row) => {
         const [date, close] = row.split(",");
@@ -62,18 +66,42 @@ const createGraph = async () => {
     const minDate = d3.min(parsedData, (d) => d.date);
     const maxDate = d3.max(parsedData, (d) => d.date);
     if (minDate && maxDate) {
-        x.domain([minDate, maxDate]);
+        xScale.domain([minDate, maxDate]);
     }
 
     const maxClose = d3.max(parsedData, (d) => d.close);
     if (maxClose) {
-        y.domain([0, maxClose]);
+        yScale.domain([0, maxClose]);
     }
 
+    // canvas
+    const svg = d3
+        .select("#chart")
+        .html("")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // lines
     svg.append("path").data([parsedData]).attr("class", "line").attr("d", valueLine);
 
-    svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(x));
-    svg.append("g").call(d3.axisLeft(y));
+    // dots
+    svg.append("g")
+        .attr("class", "dots")
+        .selectAll("circle")
+        .data(parsedData)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => (d?.date ? xScale(d.date) : 0))
+        .attr("cy", (d) => yScale(d.close))
+        .attr("r", 5)
+        .attr("fill", "#69b3a2");
+
+    // axis
+    svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(xScale));
+    svg.append("g").call(d3.axisLeft(yScale));
 };
 
 const App = () => {
